633da02d9ad5982411279291ae588525f96ade45
cigix <cigix@cigix.me>
2023-01-25 21:23:01 +0100

    Implement decompression

---
+++ src/bin/unezlz.rs
@@ -0,0 +1,27 @@
+use ezlz2::buffers;
+use ezlz2::dict;
+
+fn main() {
+    let (mut input, mut output) = ezlz2::parse_args();
+
+    let mut ib = buffers::InBitsBuffer::from(&mut input);
+    let mut ob = buffers::OutBytesBuffer::from(&mut output);
+    let mut dict = dict::Dictionary::new_prefilled();
+    let mut work: Vec<u8> = Vec::new();
+
+    while let Some(value) = ib.read() {
+        if let Some(entry) = dict.get(value) {
+           if ! work.is_empty() {
+               work.push(*entry.get(0).unwrap());
+               dict.add(&work);
+           }
+           work = entry.clone();
+        } else {
+            work.push(*work.last().unwrap());
+            dict.add(&work);
+        }
+        ob.write(&work);
+    }
+
+    ob.flush();
+}
--- src/buffers.rs
+++ src/buffers.rs
@@ -121,3 +121,139 @@ impl<'a> OutBitsBuffer<'a> {
         self.writebits(value);
     }
 }
+
+/// A buffer to read bits from.
+pub struct InBitsBuffer<'a> {
+    buffer: [u8; BUFFER_SIZE],
+    size: usize,
+    // an index into the buffer, to the partial byte being read
+    byteposition: usize,
+    bitposition: u32, // an index into the current byte
+    bitlength: u32,
+    input: &'a mut dyn std::io::Read
+}
+
+impl<'a> InBitsBuffer<'a> {
+    pub fn from(input: &'a mut dyn std::io::Read) -> Self
+    {
+        let mut ib = InBitsBuffer {
+            buffer: [0; BUFFER_SIZE],
+            size: 0,
+            byteposition: 0,
+            bitposition: 0,
+            bitlength: u8::BITS,
+            input
+        };
+        ib.populate();
+        ib
+    }
+
+    fn populate(&mut self)
+    {
+        self.size = self.input.read(&mut self.buffer).unwrap();
+    }
+
+    fn readbits(&mut self) -> Option<usize>
+    {
+        let mut value: usize = 0;
+        let mut bitlength = self.bitlength;
+        let mut readbits: u32 = 0;
+        while bitlength != 0 {
+            if self.size == 0 {
+                return None;
+            }
+            let readable = u8::BITS - self.bitposition;
+            let toread = readable.min(bitlength);
+            let mask = (1usize << toread) - 1;
+
+            let curbyte = self.buffer[self.byteposition] as usize;
+            let curbits = (curbyte >> self.bitposition) & mask;
+            value |= curbits << readbits;
+
+            self.bitposition += toread;
+            if self.bitposition == u8::BITS {
+                self.bitposition = 0;
+                self.byteposition += 1;
+                if self.byteposition == self.size {
+                    self.populate();
+                    self.byteposition = 0;
+                }
+            }
+
+            bitlength -= toread;
+            readbits += toread;
+        }
+
+        Some(value)
+    }
+
+    pub fn read(&mut self) -> Option<usize>
+    {
+        if self.size == 0 {
+            return None;
+        }
+
+        let value = self.readbits()?;
+        if ESCAPE as usize == value {
+            let nextvalue = self.readbits()?;
+            if ESCAPE as usize != nextvalue {
+                // Single escape: increase window size and try again
+                self.bitlength += 1;
+                return self.read();
+            }
+            // Double escape: return the escape value
+        }
+
+        Some(value)
+    }
+}
+
+/// A buffer to write bytes to.
+pub struct OutBytesBuffer<'a> {
+    buffer: [u8; BUFFER_SIZE],
+    position: usize, // an index into the buffer
+    output: &'a mut dyn std::io::Write
+}
+
+impl<'a> OutBytesBuffer<'a> {
+    pub fn from(output: &'a mut dyn std::io::Write) -> Self
+    {
+        OutBytesBuffer {
+            buffer: [0; BUFFER_SIZE],
+            position: 0,
+            output
+        }
+    }
+
+    fn writeout(&mut self)
+    {
+        self.output.write(&self.buffer[0..self.position]).unwrap();
+        self.position = 0;
+    }
+
+    pub fn flush(&mut self)
+    {
+        self.writeout();
+        self.output.flush().unwrap();
+    }
+
+    pub fn write(&mut self, bytes: &[u8])
+    {
+        let space = self.buffer.len() - self.position;
+        if bytes.len() <= space {
+            // Enough space for bytes: copy bytes entirely
+            self.buffer[self.position..self.position + bytes.len()]
+                .copy_from_slice(bytes);
+            self.position += bytes.len();
+            if self.position == self.buffer.len() {
+                self.writeout();
+            }
+        } else {
+            // Not enough space for bytes: copy the prefix
+            self.buffer[self.position..].copy_from_slice(&bytes[..space]);
+            self.writeout();
+            // Try again with the suffix
+            self.write(&bytes[space..]);
+        }
+    }
+}
